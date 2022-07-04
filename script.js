9878a00a71b599b9d94f0e48ced7981fb0a90422
elshora97 <ahmedashrafrlshora@gmail.com>
2022-07-05 00:21:21 +0200

    poject functionality

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
 const endgameEl = document.getElementById('end-game-container');
-const settingsBtn = document.getElementById('settings-btn');
-const settings = document.getElementById('settings');
-const settingsForm = document.getElementById('settings-form');
+const settingsBtn = document.getElementById('setting-btn');
+const settings = document.getElementById('setting');
+const settingsForm = document.getElementById('setting-form');
 const difficultySelect = document.getElementById('difficulty');
 
 // List of words for game
@@ -19,6 +19,10 @@ let time = 10;
 text.focus();
 
 const timeInterval = setInterval(updateTime,1000);
+
+let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
+difficultySelect.value = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
 
 function getRandomWord(){
     return words[Math.floor(Math.random()* words.length)];
@@ -39,7 +43,23 @@ function updateScore() {
 function updateTime(){
     time--;
     timeEl.innerHTML = time + 's';
-    
+
+    if(time === 0) {
+        clearInterval(timeInterval)
+
+        //end game
+        gameOver();
+    }
+}
+
+function gameOver() {
+    endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+    `
+
+    endgameEl.style.display = 'flex';
 }
 
 
@@ -55,5 +75,30 @@ text.addEventListener('input', e => {
         updateScore();
 
         e.target.value = ''
+        
+        if(difficulty == 'hard'){
+            time += 2;
+            updateTime();
+        }else if(difficulty == 'medium'){
+            time += 3;
+            updateTime();
+        }else {
+            time += 5;
+            updateTime();
+        }
+    
+
+        
     };
 })
+
+// settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+settingsBtn.addEventListener('click', () => 
+settings.classList.toggle('hide'));
+
+settingsForm.addEventListener('change' , e => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty',difficulty);
+})
+
